20cb141c2843f6c8ecacea593370fe7aada981a3
Hussam Soufi <hussam1soufi@gmail.com>
2020-03-13 18:05:01 +0100

    Update touch.py

--- touch.py
+++ touch.py
@@ -1,7 +1,7 @@
 #Copyright (c) 03.2020 @author  Hussam Soufi: hussam1soufi@gmail.com
 
 #when you run the python script the camera will take a photo
-#you need to choose two boxes (face & hand) that you don't want to collapse!
+#you need to make a box around your hand. the face will be detected automaticlly
 #Then press Enter to start tracking and ESC to Exit
 #p.s: choose your face at first and then your hand for better results!
 #Have fun and stay healthy. Cheers! - Sam
