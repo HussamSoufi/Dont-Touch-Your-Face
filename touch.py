a8ff6b2a8ce1b8d97b8079426e1896d6d296bf0c
Hussam Soufi <hussam1soufi@gmail.com>
2020-03-13 13:18:14 +0100

    edit touch

--- touch.py
+++ touch.py
@@ -3,6 +3,7 @@
 #when you run the python script the camera will take a photo
 #you need to choose two boxes (face & hand) that you don't want to collapse!
 #Then press Enter to start tracking and ESC to Exit
+#p.s: choose your face at first and then your hand for better results!
 #Have fun and stay healthy. Cheers! - Sam
 
 import cv2 #pip install opencv-contrib-python
